4901aab2fe0d9bef5e47f550465928b980c681b1
DayaneCordeiro <dayane.cordeirogs@gmail.com>
2021-10-19 10:43:00 -0300

    Feature Including Github and CallBack Function Routes

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,21 @@
+import "dotenv/config";
 import express from "express";
 
 const app = express();
+
+// Route to github login
+app.get("/github", (request, response) => {
+  response.redirect(
+    `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`
+  );
+});
+
+// Route to callback function
+app.get("/signin/callback", (request, response) => {
+  const { code } = request.query;
+
+  return response.json(code);
+});
 
 // Application will run on 4000 port
 app.listen(4000, () => console.log(`🚀 Server is running on 4000 PORT`));
