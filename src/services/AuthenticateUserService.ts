133de370dbbb1d04ff5e3c9f7be01f53d4f840e4
DayaneCordeiro <dayane.cordeirogs@gmail.com>
2021-10-19 14:14:42 -0300

    Feature Creating User and Creating Users Token

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,10 +1,12 @@
 import axios from "axios";
+import prismaClient from "../prisma";
+import { sign } from "jsonwebtoken";
 
 /**
  * Receber code (string)
- * Recuperar o acces_token no github
- * Recuperar infos do user no Github
- * Verificar se o user existe no DB
+ * Recuperar o acces_token no github      ✔
+ * Recuperar infos do user no Github      ✔
+ * Verificar se o user existe no DB       ✔
  * ----- SIM = Gera um token
  * ----- NÃO = Cria no DB, gera um token
  * Retornar o token com as infos do user
@@ -48,7 +50,43 @@ class AuthenticateUserService {
       }
     );
 
-    return response.data;
+    const { login, id, avatar_url, name } = response.data;
+
+    // Search for user
+    let user = await prismaClient.user.findFirst({
+      where: {
+        github_id: id,
+      },
+    });
+
+    // If user not exists, create
+    if (!user) {
+      user = await prismaClient.user.create({
+        data: {
+          github_id: id,
+          login,
+          avatar_url,
+          name,
+        },
+      });
+    }
+
+    const token = sign(
+      {
+        user: {
+          name: user.name,
+          avatar_url: user.avatar_url,
+          id: user.id,
+        },
+      },
+      process.env.JWT_SECRET,
+      {
+        subject: user.id,
+        expiresIn: "1d",
+      }
+    );
+
+    return { token, user };
   }
 }
 
