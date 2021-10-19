2ba4d0d40414a26612d3a4024d17c7c8c5572f64
DayaneCordeiro <dayane.cordeirogs@gmail.com>
2021-10-19 17:16:52 -0300

    Feature Create Message Route

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
-import { AuthenticateUserService } from "./services/AuthenticateUserService";
+import { CreateMessageController } from "./controllers/CreateMessageController";
+import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
 
 const router = Router();
 
 router.post("/authenticate", new AuthenticateUserController().handle);
+
+router.post(
+  "/messages",
+  ensureAuthenticated,
+  new CreateMessageController().handle
+);
 
 export { router };
