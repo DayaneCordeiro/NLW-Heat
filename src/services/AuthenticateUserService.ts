98da54286e2f3acf73796e90af4bf54feb9af412
DayaneCordeiro <dayane.cordeirogs@gmail.com>
2021-10-19 11:38:42 -0300

    Feature Routes To GitHub on Services Configurations

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,15 +1,54 @@
+import axios from "axios";
+
 /**
  * Receber code (string)
  * Recuperar o acces_token no github
+ * Recuperar infos do user no Github
  * Verificar se o user existe no DB
  * ----- SIM = Gera um token
  * ----- NÃO = Cria no DB, gera um token
  * Retornar o token com as infos do user
  */
 
+interface IAccessTokenResponse {
+  access_token: string;
+}
+
+interface IUserResponse {
+  avatar_url: string;
+  login: string;
+  id: number;
+  name: string;
+}
+
 class AuthenticateUserService {
   async execute(code: string) {
-    //
+    // Calling Gihub with axios
+    const url = "https://github.com/login/oauth/access_token";
+
+    const { data: accessTokenResponse } =
+      await axios.post<IAccessTokenResponse>(url, null, {
+        params: {
+          client_id: process.env.GITHUB_CLIENT_ID,
+          client_secret: process.env.GITHUB_CLIENT_SECRET,
+          code,
+        },
+        headers: {
+          Accept: "application/json",
+        },
+      });
+
+    // Get all informations from github of logged user
+    const response = await axios.get<IUserResponse>(
+      "https://api.github.com/user",
+      {
+        headers: {
+          authorization: `Bearer ${accessTokenResponse.access_token}`,
+        },
+      }
+    );
+
+    return response.data;
   }
 }
 
