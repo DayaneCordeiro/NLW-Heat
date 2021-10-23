c50818172cd694f23c45d0dc8db4f9e9e6d70d9b
DayaneCordeiro <dayane.cordeirogs@gmail.com>
2021-10-23 16:11:26 -0300

    Feature Including Initial Struture

--- nlwheatapp/App.tsx
+++ nlwheatapp/App.tsx
@@ -1,7 +1,29 @@
 import React from "react";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import AppLoading from "expo-app-loading";
+import { StatusBar } from "expo-status-bar";
 
 import { Home } from "./src/screens/Home";
 
 export default function App() {
-  return <Home />;
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
+  // se as fontes ainda não foram carregadas, ele segura a tela de splash (tela inicial do app)
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <>
+      <StatusBar style="light" />
+      <Home />
+    </>
+  );
 }
