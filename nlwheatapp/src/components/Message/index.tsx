ed41930d0c5e9e2b66f0aad4a0189433ee2e45f6
DayaneCordeiro <dayane.cordeirogs@gmail.com>
2021-11-11 11:17:33 -0300

    NLWHeat :: Enhancement Message Component Structure

--- nlwheatapp/src/components/Message/index.tsx
+++ nlwheatapp/src/components/Message/index.tsx
@@ -5,18 +5,28 @@ import { UserPhoto } from "../UserPhoto";
 
 import { styles } from "./styles";
 
-export function Message() {
+export type MessageProps = {
+  id: string;
+  text: string;
+  user: {
+    name: string;
+    avatar_url: string;
+  };
+};
+
+type Props = {
+  data: MessageProps;
+};
+
+export function Message({ data }: Props) {
   return (
     <View style={styles.container}>
-      <Text style={styles.message}>Texto da mensagem</Text>
+      <Text style={styles.message}>{data.text}</Text>
 
-      <View>
-        <UserPhoto
-          sizes="SMALL"
-          imageUri="https://github.com/BrendaRodriguees.png"
-        />
+      <View style={styles.footer}>
+        <UserPhoto sizes="SMALL" imageUri={data.user.avatar_url} />
 
-        <Text style={styles.userName}>Brenda Cordeiro</Text>
+        <Text style={styles.userName}>{data.user.name}</Text>
       </View>
     </View>
   );
